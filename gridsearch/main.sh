0a3824effb0c8108cbd27500241ec93c0b134d1e
anhvu <lequyanh@fel.cvut.cz>
2019-11-01 10:50:24 +0100

    minor script adjustments

--- gridsearch/main.sh
+++ gridsearch/main.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
+ROOT="lequyanh@skirit.metacentrum.cz:/storage/praha1/home/lequyanh"
+
 # ----- CHOOSE A SHROOM FOR GRID SEARCH TO INFER THE BEST SET OF SVM PARAMETERS FOR ACTUAL TRAINING ------
 # Shuffle the examples and split them to train and validation sets.
 # The train set will be used for grid search training
+site="donor"
 
-shroom_csv=../data/train/donor/Armme1_1-donor-windows.csv
+shroom="Armme1_1"
+shroom_csv=../data/train/${site}/${shroom}-donor-windows.csv
+
 bash split_csv_valid_train.sh $shroom_csv
-# this will produce files with prefix 'shuffeled_train_' and 'shuffeled_valid_'
+# this will produce files with prefix 'shuffeled_train_' and 'shuffeled_valid_'. Upload them to metacenter
+scp shuffeled_train_${shroom}-${site}-windows.csv ${ROOT}/data/${site}s
 
-# Now perform grid search (on metacentre)
-site=donor
+# Now perform grid search (on metacenter)
+bash ./grid_search $site "shuffeled_train_${shroom_csv}"
 
-bash ./grid_search $site "shuffeled_train_$shroom_csv"
 # this will produce model files. Download them
-model_folder=${site}_gridmodels
-mkdir -p ./$model_folder
+models_folder=${site}_gridmodels
+mkdir -p ./$models_folder
+scp ${ROOT}/results/train-splice-site-shuffeled_train_${shroom}-${site}* $models_folder
 
-scp lequyanh@skirit.metacentrum.cz:/storage/praha1/home/lequyanh/results/train-splice-site-shuffeled_train_Armme1_1-${site}* $model_folder
+# and evaluate their performance on validation dataset. Note the parameters of the best performing one
+bash validate_grid_models.sh $site "shuffeled_valid_$shroom_csv" $models_folder
 
-bash validate_grid_models.sh $site "shuffeled_valid_$shroom_csv" $model_folder
+# train the overall model with the discovered parameters (on metacenter)
+qsub -l walltime=24:0:0 -l select=1:ncpus=10:mem=4gb:scratch_local=2gb -v DEGREE=30,LWINDOW=70,RWINDOW=70,C=1,SITE=donors,DATAFILE=data.csv,CPU=10 train-splice-site.sh
+# retrieve the model
 
+# validate the model using the mixed shuffeled dataset (created during preprocessing step)
+scp ../data/test/shuff_aggreg_donor_site_test.csv lequyanh@skirit.metacentrum.cz:/~
+qsub -l walltime=24:0:0 -l select=1:ncpus=10:mem=4gb:scratch_local=2gb -v VALIDATION_CSV=shuff_aggreg_donor_site_test.csv,MODEL=model.hd5,WIN_IN=70,SUBJECT=$site classify-splice-site.sh
