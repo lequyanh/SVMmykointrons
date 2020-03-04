fd129dd7823e3ad0e42d283202533e43a5c7b18c
anhvu <lequyanh@fel.cvut.cz>
2020-03-04 13:35:48 +0100

    comparison RMD and PY

--- comparison/compare-splice-site-classification.py
+++ comparison/compare-splice-site-classification.py
@@ -1,66 +1,71 @@
 import pandas as pd
+import intragen_tools
+
+NEWSEQUENCES_LOC = '/home/anhvu/Desktop/mykointrons-data/new-sequences'
+DONOR_SITE = 'donor'
+ACCEPTOR_SITE = 'acceptor'
 
 
 def main():
     """
     Compares splice site classification output of two models (usually NN and SVM, but can be 2 SVMs). Examines, how much
     classification is shared and whether the models tend to do similar mistakes.
-    @nn_output  CSV output of neural network splice site classification. Columns [sequence, label, prediction]
-    @svm_output CSV output of SVM splice site classification. Columns [sequence, label, prediction]
-    @nn_adjustment  Switch to adjust NN windows to have the same length as SVM windows. Needed for joining of CSVs
-
-    The files can be obtained via e.g.:
-    python classify-splice-sites.py $FILE ../pipeline/bestmodels/ascomycota/model.hd5 70 70 acceptor \
-                                -r 0.003  -c 12  > 'svm_acceptor_output.csv'
-    :return: None
+    :return:
     """
-    nn_output = f'nn_acceptor_output.csv'
-    svm_output = f'svm_acceptor_output.csv'
-    nn_adjustment = True
+    shroom_name = 'Kocim1'
+    site = 'donor'
+    model1 = 'SVM-intragen'
+    model2 = 'NN'
 
-    nn_classification = pd.read_csv(nn_output, delimiter=';')
-    svm_classification = pd.read_csv(svm_output, delimiter=';')
+    exon_csv = f'{NEWSEQUENCES_LOC}/{shroom_name}/{shroom_name}_exon_positions.csv'
+    introns_fasta = f'{NEWSEQUENCES_LOC}/{shroom_name}/{shroom_name}-introns.fasta'
 
-    if nn_adjustment:
-        # Adjust range so the sequences of two files are equally long
-        nn_classification['sequence'] = nn_classification[['sequence']].applymap(lambda seq: seq[128:270])
+    dataset_csv = f'../intragen/{shroom_name}/{shroom_name}-splice-site-{site}-dataset.csv'
+    model1_result_csv = f'../intragen/{shroom_name}/{shroom_name}-splice-site-{site}-result-{model1}.csv'
+    model2_result_csv = f'../intragen/{shroom_name}/{shroom_name}-splice-site-{site}-result-{model2}.csv'
 
-    nn_classification.drop_duplicates(inplace=True)
-    svm_classification.drop_duplicates(inplace=True)
+    merged_df1 = intragen_tools.merge_classification_data(
+        dataset_csv,
+        model1_result_csv,
+        introns_fasta,
+        exon_csv,
+        DONOR_SITE
+    )
 
-    joined_results = pd.merge(nn_classification, svm_classification, on='sequence')
-    positives_df = joined_results[joined_results['label_x'] == 1]
+    merged_df2 = intragen_tools.merge_classification_data(
+        dataset_csv,
+        model2_result_csv,
+        introns_fasta,
+        exon_csv,
+        DONOR_SITE
+    )
 
-    FNs_x = positives_df[positives_df['pred_x'] == -1].shape[0]
-    FNs_y = positives_df[positives_df['pred_y'] == -1].shape[0]
+    joined_results = pd.merge(merged_df1, merged_df2, on=['scaffold', 'start', 'label'])
+    positives_df = joined_results[joined_results['label'] == 1]
+    TPs_x = positives_df[positives_df['prediction_x'] == 1].shape[0]
+    TPs_y = positives_df[positives_df['prediction_y'] == 1].shape[0]
 
-    TPs_x = positives_df[positives_df['pred_x'] == 1].shape[0]
-    TPs_y = positives_df[positives_df['pred_y'] == 1].shape[0]
-
-    tp_tp_intersect = positives_df.query('pred_x == 1 & pred_y == 1').shape[0]
+    tp_tp_intersect = positives_df.query('prediction_x == 1 & prediction_y == 1').shape[0]
     print(f'True positives intersection {tp_tp_intersect}. '
           f'I.e {100 * tp_tp_intersect / TPs_x:.2f}% of model X '
           f'and {100 * tp_tp_intersect / TPs_y:.2f}% of model Y results')
 
-    fn_fn_intersect = positives_df.query('pred_x == -1 & pred_y == -1').shape[0]
-    print(f'False negatives intersection {fn_fn_intersect}. '
-          f'The intersection makes {100 * fn_fn_intersect / FNs_x:.2f}% of model X false negatives '
-          f'and {100 * fn_fn_intersect / FNs_y:.2f}% of model Y false negatives')
+    negatives_df = joined_results[joined_results['label'] == -1]
+    FPs_x = negatives_df[negatives_df['prediction_x'] == 1].shape[0]
+    FPs_y = negatives_df[negatives_df['prediction_y'] == 1].shape[0]
 
-    fn_tp_intersect = positives_df.query('pred_x == -1 & pred_y == 1').shape[0]
-    print(f'FN vs TP intersection {fn_tp_intersect}. '
-          f'I.e {100 * fn_tp_intersect / FNs_x:.2f}% of Xs model FN is found by model Y ')
-
-    tp_fn_intersect = positives_df.query('pred_x == 1 & pred_y == -1').shape[0]
-    print(f'FN vs TP intersection {tp_fn_intersect}. '
-          f'I.e {100 * tp_fn_intersect / FNs_y:.2f}% of Ys model FN is found by model X ')
-
-    negatives_df = joined_results[joined_results['label_x'] == -1]
-    FPs_x = negatives_df[negatives_df['pred_x'] == 1].shape[0]
-    FPs_y = negatives_df[negatives_df['pred_y'] == 1].shape[0]
-
-    fp_fp_intersect = negatives_df.query('pred_x == 1 & pred_y == 1').shape[0]
+    fp_fp_intersect = negatives_df.query('prediction_x == 1 & prediction_y == 1').shape[0]
     print(f'False positives intersection {fp_fp_intersect}. '
+          f'The intersection makes {100 * fp_fp_intersect / FPs_x:.2f}% of model X false positives '
+          f'and {100 * fp_fp_intersect / FPs_y:.2f}% of model Y false positives')
+
+    FPs_x = negatives_df.query('prediction_x == 1 and in_exon_x == 1').shape[0]
+    FPs_y = negatives_df.query('prediction_y == 1 and in_exon_y == 1').shape[0]
+
+    fp_fp_intersect = negatives_df.query(
+        'prediction_x == 1 & prediction_y == 1 & in_exon_x == 1 & in_exon_y == 1'
+    ).shape[0]
+    print(f'Intragenic false positives intersection {fp_fp_intersect}. '
           f'The intersection makes {100 * fp_fp_intersect / FPs_x:.2f}% of model X false positives '
           f'and {100 * fp_fp_intersect / FPs_y:.2f}% of model Y false positives')
 
--- intragen/intragen_splice_sites_fp.py
+++ intragen/intragen_splice_sites_fp.py
@@ -1,10 +1,6 @@
 import logging
 
-import pandas as pd
-from pandas import DataFrame
-
 import intragen_tools
-from extract_tools import true_donorac_positions
 
 NEWSEQUENCES_LOC = '/home/anhvu/Desktop/mykointrons-data/new-sequences'
 DONOR_SITE = 'donor'
@@ -59,7 +55,7 @@ def intragenic_splicesite_fpr(
     Does so by merging classification data (windows and their positions) with their classification and labels.
     Lastly, extends the merged DataFrame with information, whether the candidate lies inside exons or not.
     """
-    merged_df = merge_classification_data(
+    merged_df = intragen_tools.merge_classification_data(
         classification_data_csv,
         classification_result_csv,
         introns_fasta,
@@ -79,106 +75,6 @@ def intragenic_splicesite_fpr(
           f'from which intra-genic FPs {intra_exon_fps} (intra-genic FP ratio {intragen_fpr:.2f}%)\n')
 
     return intragen_fpr
-
-
-def merge_classification_data(
-        classification_data_csv,
-        classification_result_csv,
-        introns_fasta,
-        exon_csv,
-        site: str
-) -> DataFrame:
-    classification_data_df = pd.read_csv(classification_data_csv, delimiter=';')
-    # Remove unnecessary sequence column
-    classification_data_df.drop(columns='sequence', inplace=True)
-
-    merged_df = merge_data_prediction_labels(
-        classification_data_df,
-        classification_result_csv,
-        introns_fasta,
-        site
-    )
-
-    merged_df.rename(columns={"position": "start"}, inplace=True)  # Rename so it matches required interface
-    merged_df = add_in_exon_column(merged_df, exon_csv)
-
-    return merged_df
-
-
-def merge_data_prediction_labels(
-        classification_data_df: DataFrame,
-        classification_result_csv: str,
-        introns_fasta: str,
-        site: str
-) -> DataFrame:
-    """
-    Merges donor/acceptor dataset with predictions and labels.
-    The goal is to have attributes 'scaffold', 'position', 'prediction', 'label' for each candidate window
-
-    :param classification_data_df: DataFrame of candidate splice site (windows) ['scaffold', 'position']
-    :param classification_result_csv: CSV file with positively classified splice site positions ['scaffold', 'position']
-    :param introns_fasta: FASTA file with introns
-    :param site: Donor/Acceptor
-    :return: Merged DataFrame with ['scaffold', 'position', 'prediction', 'label']
-    """
-    classification_result_df = pd.read_csv(classification_result_csv, delimiter=';')
-    # Add new column and set it to be ones
-    classification_result_df['prediction'] = 1
-
-    # Create a DF with true donor/acceptor positions (i.e. DataFrame of candidate labels)
-    true_donor_pos, true_acceptor_pos = true_donorac_positions(introns_fasta)
-
-    if site == DONOR_SITE:
-        true_splicesite_df = intragen_tools.splice_site_positions_dict_to_df(true_donor_pos)
-    else:
-        true_splicesite_df = intragen_tools.splice_site_positions_dict_to_df(true_acceptor_pos)
-    # Add new column and set it to be ones
-    true_splicesite_df['label'] = 1
-
-    # Merge all the 3 data sets (input dataset, classification and labels) together
-    merged_df = classification_data_df.merge(true_splicesite_df, how="left").fillna(-1)
-    merged_df = merged_df.merge(classification_result_df, how="left").fillna(-1)
-
-    return merged_df
-
-
-def add_in_exon_column(
-        merged_df: DataFrame,
-        exon_csv: str
-) -> DataFrame:
-    """
-    Extends a given DataFrame of candidate splice sites with column 'in_exon'.
-
-    :param merged_df: DataFrame with columns ['scaffold', 'start'], preferably also with ['prediction', 'label']
-    :param exon_csv: CSV with exon positions
-    :return: Merged DataFrame with ['scaffold', 'start', 'prediction', 'label', 'in_exon']
-    """
-    merged_df['in_exon'] = -1  # New column of "in_exon" indicators. Initialize to -1 and update those inside exons
-
-    # Create DataFrame with exon positions. The DF will be used to determine intra-exonic false predictions
-    exon_pos_df = pd.read_csv(exon_csv, delimiter=';')
-
-    exon_grouped = exon_pos_df.groupby(by='scaffold')
-    merged_grouped = merged_df.groupby(by='scaffold')
-
-    for scaffold, positions in merged_grouped:
-        try:
-            scaff_exon_positions = exon_grouped.get_group(scaffold)
-        except KeyError:
-            logging.warning(f'No exons in scaffold {scaffold}, or scaffold missing')
-            continue
-
-        # Pick positions (within given scaffold) of false positives and find, whether they fall into exon or not
-        filter_false_positives = (merged_df.scaffold == scaffold) & (merged_df.label == -1) & (
-                merged_df.prediction == 1)
-        cut_positions = merged_df[filter_false_positives]
-        exon_cut_indicators, _ = intragen_tools.intraexonic_cuts_in_scaffold(scaff_exon_positions, cut_positions)
-
-        merged_df.loc[filter_false_positives, 'in_exon'] = list(
-            map(lambda x: -1 if x is False else 1, exon_cut_indicators)
-        )
-
-    return merged_df
 
 
 if __name__ == '__main__':
